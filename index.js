f46298fb9d1d2e0bb5d098061c9b91f599368264
Mikola Lysenko <mikolalysenko@gmail.com>
2013-01-15 10:36:32 -0600

    Adding files

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.surfaceNets         = require("./lib/surfacenets.js").surfaceNets;
+exports.marchingCubes       = require("./lib/marchingcubes.js").marchingCubes;
+exports.marchingTetrahedra  = require("./lib/marchingtetrahedra.js").marchingTetrahedra;
